5cc5521b7147173e143520c93fddccdb1f146c00
Basavesh <basavesh1308@gmail.com>
2020-12-28 19:10:55 +0530

    Revert "committing Spring boot CRUD with MQSQL integrated with swagger2"
    This reverts commit 7f7844ed

--- src/main/java/com/uk/springbootcrud/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.uk.springbootcrud;
-
-public class AppConstants {
-    public static final String SUCCESS = "SUCCESS";
-    public static final String ERROR = "ERROR";
-}
--- src/main/java/com/uk/springbootcrud/exception/NoDepartmentDataException.java
+++
@@ -1,7 +0,0 @@
-package com.uk.springbootcrud.exception;
-
-public class NoDepartmentDataException extends RuntimeException {
-    public NoDepartmentDataException(String msg){
-        super(msg);
-    }
-}
